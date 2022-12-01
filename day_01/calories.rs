9b736a8bf2f17d1f3f541fccb92c49b92c24626d
Moi <oxspit@gmail.com>
2022-12-01 13:41:28 +0100

    day 1 part 2

--- day_01/calories.rs
+++ day_01/calories.rs
@@ -7,3 +7,19 @@ pub fn get_calories_per_elf(calorie_list: &str) -> Vec<i32> {
 
     return elf_calories;
 }
+
+pub fn get_top_three_calories(calorie_list: Vec<i32>) -> Vec<i32> {
+    let mut top_three = vec![0; 5];
+
+    for calories in calorie_list.iter() {
+        for index in 0..3 {
+            if calories > &top_three[index] {
+                top_three.insert(index, *calories);
+                top_three.resize(3, 0);
+                break;
+            };
+        }
+    }
+
+    return top_three;
+}
--- day_01/main.rs
+++ day_01/main.rs
@@ -1,14 +1,42 @@
 mod calories;
 use calories::get_calories_per_elf;
+use calories::get_top_three_calories;
 
-fn main() {
+fn part_one() {
+    println!("PART 1");
     println!("Test input");
     let calories_per_elf_test = get_calories_per_elf(include_str!("test_input.txt"));
 
     println!("The elf with more calories has {:?} calories", calories_per_elf_test.iter().max().unwrap());
-    println!("-------------");
+    println!("-------------\n");
 
-    println!("Real input part #1");
+    println!("Real input");
     let calories_per_elf = get_calories_per_elf(include_str!("day_01_input.txt"));
     println!("The elf with more calories has {:?} calories", calories_per_elf.iter().max().unwrap());
+    println!("-------------\n\n");
+}
+
+fn part_two() {
+    println!("PART 2");
+    println!("Test input");
+    let calories_per_elf_test = get_calories_per_elf(include_str!("test_input.txt"));
+    let top_three_calories_test = get_top_three_calories(calories_per_elf_test);
+
+    println!("The three elves with the most calories have {:?} calories respectively", top_three_calories_test);
+    println!("This adds up to {:?}", top_three_calories_test.iter().fold(0, |sum, calories| sum + calories));
+    println!("-------------\n");
+
+    println!("Real input");
+    let calories_per_elf = get_calories_per_elf(include_str!("day_01_input.txt"));
+    let top_three_calories = get_top_three_calories(calories_per_elf);
+
+    println!("The three elves with the most calories have {:?} calories respectively", top_three_calories);
+    println!("This adds up to {:?}", top_three_calories.iter().fold(0, |sum, calories| sum + calories));
+    println!("-------------\n\n");
+}
+
+fn main() {
+    part_one();
+
+    part_two();
 }
