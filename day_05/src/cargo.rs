6666b6ad9609535dd4f081546cd37e781c6d01df
Moi <oxspit@gmail.com>
2022-12-05 21:26:46 +0100

    day 05 part 2

--- day_05/src/cargo.rs
+++ day_05/src/cargo.rs
@@ -23,18 +23,28 @@ pub fn get_cargo_set(cargo_as_str: &str) -> Vec<Vec<char>> {
     return cargo;
 }
 
-pub fn apply_instructions(mut file: CargoInstructions) -> CargoInstructions {
+fn parse_instructions(instruction: &str) -> Vec<usize> {
+    return instruction
+        .split_ascii_whitespace()
+        .filter(|s| s.chars().all(char::is_numeric))
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
+}
+
+pub fn apply_instructions(mut file: CargoInstructions, crate_moover_9001: bool) -> CargoInstructions {
     for instruction in file.instructions.lines() {
-        let instructions_parsed: Vec<usize> = instruction
-            // .split_inclusive(char::is_numeric)
-            .split_ascii_whitespace()
-            .filter(|s| s.chars().all(char::is_numeric))
-            .map(|s| s.parse::<usize>().unwrap())
-            .collect();
-        println!("{:?}", instructions_parsed);
+        let instructions_parsed: Vec<usize> = parse_instructions(instruction);
+        let mut moved_objects = Vec::new();
+
         for _ in 0..(instructions_parsed[0]) {
             let popped_value = file.cargo[instructions_parsed[1] - 1].pop().unwrap();
-            file.cargo[instructions_parsed[2] - 1].push(popped_value);
+            moved_objects.push(popped_value);
+        }
+
+        if crate_moover_9001 { moved_objects.reverse() }
+
+        for i in 0..(moved_objects.len()) {
+            file.cargo[instructions_parsed[2] - 1].push(moved_objects[i]);
         }
     }
 
--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -3,13 +3,11 @@ use cargo::apply_instructions;
 
 mod files;
 use files::test_file;
+use files::day_file;
 
-use crate::files::day_file;
-// use files::day_file;
-
-fn part_one(test: bool) {
+fn part_one(test: bool, crate_moover_9001: bool) {
     let mut file = if test {test_file()} else {day_file()};
-    file = apply_instructions(file);
+    file = apply_instructions(file, crate_moover_9001);
 
     let mut final_str = String::from("");
 
@@ -22,6 +20,10 @@ fn part_one(test: bool) {
 
 fn main() {
     println!("## PART 1 ##");
-    part_one(true);
-    part_one(false);
+    part_one(true, false);
+    part_one(false, false);
+
+    println!("## PART 2 ##");
+    part_one(true, true);
+    part_one(false, true);
 }
