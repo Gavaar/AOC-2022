096365bb8152ab977cc4ccf21c422e1cec38cec0
Moi <oxspit@gmail.com>
2022-12-11 18:25:55 +0100

    day 11 part 2

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,8 +1,8 @@
 mod files;
 mod monkeys;
 
-fn part_one_calculations(final_state_monke: Vec<monkeys::Monkey>) -> (u32, u32) {
-    let mut inspected_list = final_state_monke.iter().map(|monke| monke.inspected).collect::<Vec<u32>>();
+fn worry_calculator(final_state_monke: Vec<monkeys::Monkey>) -> (u128, u128) {
+    let mut inspected_list = final_state_monke.iter().map(|monke| monke.inspected).collect::<Vec<u128>>();
     inspected_list.sort();
 
     return (inspected_list.pop().unwrap(), inspected_list.pop().unwrap());
@@ -10,10 +10,25 @@ fn part_one_calculations(final_state_monke: Vec<monkeys::Monkey>) -> (u32, u32) 
 
 fn part_one(file: &'static str) {
     let mut monkeys = monkeys::parse_file(file);
+
     for _ in 0..20 {
         monkeys::monkey_round(&mut monkeys, true);
     }
-    let (biggest, second_biggest) = part_one_calculations(monkeys);
+
+    let (biggest, second_biggest) = worry_calculator(monkeys);
+
+    println!("biggest: {}, second: {}", biggest, second_biggest);
+    println!("their multiplied values are: {}", biggest * second_biggest);
+}
+
+fn part_two(file: &'static str) {
+    let mut monkeys = monkeys::parse_file(file);
+
+    for _ in 0..10000 {
+        monkeys::monkey_round(&mut monkeys, false);
+    }
+
+    let (biggest, second_biggest) = worry_calculator(monkeys);
 
     println!("biggest: {}, second: {}", biggest, second_biggest);
     println!("their multiplied values are: {}", biggest * second_biggest);
@@ -24,11 +39,15 @@ fn main() {
     let day_file = files::get(false);
 
     println!("### PART 1 ###");
-    println!("--> TEST\n");
+    println!("--> TEST");
     part_one(test_file);
-    println!("--> DAY\n");
+    println!("--> DAY");
     part_one(day_file);
 
     println!("--------------");
     println!("### PART 2 ###");
+    println!("--> TEST\n");
+    part_two(test_file);
+    println!("--> DAY\n");
+    part_two(day_file);
 }
--- day_11/src/monkeys.rs
+++ day_11/src/monkeys.rs
@@ -1,32 +1,32 @@
 
-fn get_starting_items(items: &str) -> Vec<u32> {
+fn get_starting_items(items: &str) -> Vec<u128> {
     let mut starting_items = items.split(": ");
     starting_items.next();
     let items = match starting_items.next() {
-        Some(v) => v.split(',').map(|item| item.trim().parse::<u32>().unwrap()).collect(),
+        Some(v) => v.split(',').map(|item| item.trim().parse::<u128>().unwrap()).collect(),
         None => Vec::new(),
     };
 
     return items;
 }
 
-fn add_by(old: u32, value: u32) -> u32 {
+fn add_by(old: u128, value: u128) -> u128 {
     old + value
 }
-fn multiply_by(old: u32, value: u32) -> u32 {
+fn multiply_by(old: u128, value: u128) -> u128 {
     old * value
 }
-fn multiply_by_itself(old: u32, _: u32) -> u32 {
+fn multiply_by_itself(old: u128, _: u128) -> u128 {
     old * old
 }
 
-fn get_operation(operation_str: &str) -> (fn(u32, u32) -> u32, u32) {
+fn get_operation(operation_str: &str) -> (fn(u128, u128) -> u128, u128) {
     let mut operation = operation_str.split("new = old ");
     operation.next();
     let (op, value) = operation.next().unwrap().split_at(1);
     let op_value = match value.trim() {
         "old" => 0,
-        v => v.parse::<u32>().unwrap(),
+        v => v.parse::<u128>().unwrap(),
     };
     let op_fn = if op_value == 0 {
         multiply_by_itself
@@ -40,27 +40,27 @@ fn get_operation(operation_str: &str) -> (fn(u32, u32) -> u32, u32) {
     return (op_fn, op_value);
 }
 
-fn get_test(test_str: &str) -> u32 {
+fn get_test(test_str: &str) -> u128 {
     let mut test = test_str.split("divisible by ");
     test.next();
-    return test.next().unwrap().parse::<u32>().unwrap();
+    return test.next().unwrap().parse::<u128>().unwrap();
 }
 
-fn get_when_case(rest: &str) -> u32 {
+fn get_when_case(rest: &str) -> u128 {
     let mut true_false_case = rest.split("throw to monkey ");
     true_false_case.next();
-    return true_false_case.next().unwrap().parse::<u32>().unwrap();
+    return true_false_case.next().unwrap().parse::<u128>().unwrap();
 }
 
 #[derive(Debug)]
 pub struct Monkey {
-    pub items: Vec<u32>,
-    pub inspected: u32,
-    pub op_fn: fn(u32, u32) -> u32,
-    pub op_value: u32,
-    pub test: u32,
-    pub true_case: u32,
-    pub false_case: u32,
+    pub items: Vec<u128>,
+    pub inspected: u128,
+    pub op_fn: fn(u128, u128) -> u128,
+    pub op_value: u128,
+    pub test: u128,
+    pub true_case: u128,
+    pub false_case: u128,
 }
 
 pub fn parse_file(file: &'static str) -> Vec<Monkey> {
@@ -82,7 +82,7 @@ pub fn parse_file(file: &'static str) -> Vec<Monkey> {
             true_case,
             false_case,
             items: starting_items,
-            inspected: 0 as u32,
+            inspected: 0 as u128,
         };
 
         monkeys.push(monkey);
@@ -92,24 +92,30 @@ pub fn parse_file(file: &'static str) -> Vec<Monkey> {
 }
 
 // send_to, new_value
-fn inspect_item(monke: &Monkey, divided_by: u32) -> Vec<(u32, u32)> {
-    let mut send_to: Vec<(u32, u32)> = Vec::new();
+fn inspect_item(monke: &Monkey, divided_by: u128, mincom: u128) -> Vec<(u128, u128)> {
+    let mut send_to: Vec<(u128, u128)> = Vec::new();
 
     for item in monke.items.clone() {
-        let new_value = (((monke.op_fn)(item, monke.op_value) / divided_by) as f64).floor() as u32;
+        let new_value = (((monke.op_fn)(item, monke.op_value) / divided_by) as f64).floor() as u128;
         let tested = (new_value % monke.test) == 0;
-        send_to.push((if tested { monke.true_case } else { monke.false_case }, new_value));
+
+        let target = if tested { monke.true_case } else { monke.false_case };
+        let value_to_push: u128 = new_value % mincom;
+
+        send_to.push((target, value_to_push));
     }
 
     return send_to;
 }
 
 pub fn monkey_round(monkeys: &mut Vec<Monkey>, part_one: bool) {
+    let min_comm_mul = monkeys.iter().fold(1, |acc, m| m.test * acc);
+
     let divided_by = if part_one { 3 } else { 1 };
     for index in 0..monkeys.len() {
         let monke = &mut monkeys[index];
-        let monkey_round = inspect_item(monke, divided_by);
-        monke.inspected = monke.inspected + monkey_round.len() as u32;
+        let monkey_round = inspect_item(monke, divided_by, min_comm_mul);
+        monke.inspected = monke.inspected + monkey_round.len() as u128;
 
         for (send_to, new_value) in monkey_round {
             monkeys[send_to as usize].items.push(new_value);
